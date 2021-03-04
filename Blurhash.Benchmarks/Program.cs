ae56ee9465d4f25f0f6b257984829744685083fe
204504bySE <204504byse@204504byse.info>
2021-03-05 03:20:18 +0900

    use BenchmarkDotNet

---
+++ Blurhash.Benchmarks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Blurhash.ImageSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Blurhash.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ImageSharpBenchmarks>();
+        }
+    }
+
+    public class ImageSharpBenchmarks
+    {
+        [Benchmark]
+        public async Task EncodeBenchmark()
+        {
+            var image = await Image.LoadAsync<Rgb24>("1233360707896238080.jpg");
+            var encoder = new Encoder(150, 150, 9, 9);
+            encoder.Encode(image, 9, 9);
+        }
+    }
+}
